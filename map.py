cb2eeff2fe72de2346b68000b7ca88d8bbfa6a47
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-17 13:58:31 -0400

    Adding Colors for Polygon based on Population

--- map.py
+++ map.py
@@ -23,6 +23,11 @@ for lt, ln, el in zip(lat,lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln],popup=str(el)+" m",radius=6,color="grey",fill_opacity=0.7, fill_color=color_producer(el)))
 
 
-fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read())))
+#fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig'), style_function=lambda x:{'fill_color':'yellow' if x['properties']['POP2005'] < 10000000}))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 map.save("Map5.html")

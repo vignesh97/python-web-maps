ea9b0e3ca81fb650839a99a1d39002ad6b6bac75
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-17 13:33:11 -0400

    Circle Marker

--- map.py
+++ map.py
@@ -8,11 +8,19 @@ elev = list(data["ELEV"])
 
 map=folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 
 fg=folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat,lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color='red')))
+    fg.add_child(folium.CircleMarker(location=[lt,ln],popup=str(el)+" m",radius=2,line_color="black", fill_color=color_producer(el)))
 
 map.add_child(fg)
-map.save("Map3.html")
+map.save("Map5.html")

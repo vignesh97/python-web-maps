c39ebc79858edd9eccb58ab3873f9d81daa1d45d
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-17 13:14:50 -0400

    Loading Valconoes dataset using pandas

--- map.py
+++ map.py
@@ -1,11 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+
 map=folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 
 
 fg=folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2,-99.1],[39.2,-97.1]]:
-    fg.add_child(folium.Marker(location=coordinates,popup="Hi I am a Marker",icon=folium.Icon(color='red')))
+for lt, ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am a Marker",icon=folium.Icon(color='red')))
 
 map.add_child(fg)
-map.save("Map1.html")
+map.save("Map2.html")

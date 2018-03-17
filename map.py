b3fca32cedcb441f44053bf2f6ef38cf233135bf
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-17 13:22:45 -0400

    Adding elevation

--- map.py
+++ map.py
@@ -4,15 +4,15 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
-
+elev = list(data["ELEV"])
 
 map=folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 
 
 fg=folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am a Marker",icon=folium.Icon(color='red')))
+for lt, ln, el in zip(lat,lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color='red')))
 
 map.add_child(fg)
-map.save("Map2.html")
+map.save("Map3.html")
